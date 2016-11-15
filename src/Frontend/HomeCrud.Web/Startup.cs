3859672ea5ff4ecbcc070562ff6ed4b927fc0a19
Dawlin Peña Luciano <dawmusic19@gmail.com>
2016-11-15 12:13:09 -0400

    Added backend and frontend folder. Added initial web project

---
+++ src/Frontend/HomeCrud.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HomeCrud.Web.Startup))]
+namespace HomeCrud.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
