01867efcc0b9205b33e3890600feaa8c282cbfff
Dawlin Peña Luciano <dawmusic19@gmail.com>
2016-11-14 21:44:31 -0400

    Rename Spec folder to Spec. Added PersonFeatures and Steps

---
+++ src/HomeCrud.Test/Features/PersonFeatures.feature
@@ -0,0 +1,25 @@
+﻿Feature: Person Features
+	In order to manage which person lives in a home
+	As a anonymous
+	I want to be able to create, read, update and delete persons that lives in a home
+
+	Background: 
+	Given I create one home with the following data
+		| Field   | Value        |
+		| Name    | Peña's Home  |
+		| Address | Test Address |
+
+Scenario: Add person to home
+	When I add a person to the last added home, with the following data
+		| Field     | Value        |
+		| FirstName | Dawlin       |
+		| LastName  | Peña Luciano |
+		| Gender    | Male         |
+		| Id        | 00128739281  |
+	Then the last person created should contain the following data
+		| Field     | Value        |
+		| FirstName | Dawlin       |
+		| LastName  | Peña Luciano |
+		| Gender    | Male         |
+		| Id        | 00128739281  |
+	And the last home created should contain a person with id '00128739281'
---
+++ src/HomeCrud.Test/Steps/PersonFeaturesSteps.cs
@@ -0,0 +1,36 @@
+﻿using SimpleInjector;
+using System;
+using TechTalk.SpecFlow;
+
+namespace HomeCrud.Test.Specs
+{
+    [Binding]
+    public class PersonFeaturesSteps
+    {
+        private readonly Scope _container = new ModuleManager().Container.BeginLifetimeScope();
+
+        [Given(@"I create one home with the following data")]
+        public void CreateHome(Table table)
+        {
+            
+        }
+
+        [When(@"I add a person to the last added home, with the following data")]
+        public void AddPersonToLastHome(Table table)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the last person created should contain the following data")]
+        public void ThenTheLastPersonCreatedShouldContainTheFollowingData(Table table)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the last home created should contain a person with id '(.*)'")]
+        public void ThenTheLastHomeCreatedShouldContainAPersonWithId(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
